03735abbc9bef0efd2fc403aa83b13a71b8ad991
Clark Rinker <cgrinker@gmail.com>
2025-12-16 17:27:20 -0800

    Implement Task 6.2 and 6.3: Test Runner and Linker

---
+++ tests/integration/math.ts
@@ -0,0 +1,22 @@
+function add(a: number, b: number) {
+    return a + b;
+}
+
+function sub(a: number, b: number) {
+    return a - b;
+}
+
+function mul(a: number, b: number) {
+    return a * b;
+}
+
+function main() {
+    console.log("Math Tests");
+    // Int
+    console.log("1 + 2 =");
+    // console.log(add(1, 2)); // TODO: Support printing numbers
+    
+    // Double
+    console.log("3.5 * 2.0 =");
+    // console.log(mul(3.5, 2.0));
+}
