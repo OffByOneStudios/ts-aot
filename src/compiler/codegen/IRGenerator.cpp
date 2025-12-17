6ffbbec2cc3ad8858eb9c64c2210aa1ab6c1bdf5
Clark Rinker <cgrinker@gmail.com>
2025-12-16 16:53:50 -0800

    Implement Task 5.2: Function Prototypes

--- src/compiler/codegen/IRGenerator.cpp
+++ src/compiler/codegen/IRGenerator.cpp
@@ -10,7 +10,32 @@ IRGenerator::IRGenerator() {
 }
 
 void IRGenerator::generate(const std::vector<Specialization>& specializations) {
-    // TODO: Implement generation logic
+    generatePrototypes(specializations);
+}
+
+llvm::Type* IRGenerator::getLLVMType(const std::shared_ptr<Type>& type) {
+    switch (type->kind) {
+        case TypeKind::Void: return llvm::Type::getVoidTy(*context);
+        case TypeKind::Int: return llvm::Type::getInt64Ty(*context);
+        case TypeKind::Double: return llvm::Type::getDoubleTy(*context);
+        case TypeKind::Boolean: return llvm::Type::getInt1Ty(*context);
+        // TODO: String, Any, Function
+        default: return llvm::Type::getVoidTy(*context);
+    }
+}
+
+void IRGenerator::generatePrototypes(const std::vector<Specialization>& specializations) {
+    for (const auto& spec : specializations) {
+        std::vector<llvm::Type*> argTypes;
+        for (const auto& argType : spec.argTypes) {
+            argTypes.push_back(getLLVMType(argType));
+        }
+
+        llvm::Type* returnType = getLLVMType(spec.returnType);
+        llvm::FunctionType* funcType = llvm::FunctionType::get(returnType, argTypes, false);
+
+        llvm::Function::Create(funcType, llvm::Function::ExternalLinkage, spec.specializedName, module.get());
+    }
 }
 
 void IRGenerator::dumpIR() {
